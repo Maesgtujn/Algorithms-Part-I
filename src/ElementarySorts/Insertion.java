ebaa5410e9b518d3642e7ce4b2f09e27026a5d5a
jimmyli-MXIC <li759847257@gmail.com>
2018-11-06 14:52:57 +0800

    add ElementarySorts

---
+++ src/ElementarySorts/Insertion.java
@@ -0,0 +1,15 @@
+package ElementarySorts;
+
+import static ElementarySorts.Selection.exch;
+import static ElementarySorts.Selection.less;
+
+public class Insertion {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+        for (int i = 0; i < N; i++)
+            for (int j = i; j > 0; j--)
+                if (less(a[j], a[j-1]))
+                    exch(a, j, j-1);
+                else break;
+    }
+}
---
+++ src/ElementarySorts/Shell.java
@@ -0,0 +1,24 @@
+package ElementarySorts;
+
+import static ElementarySorts.Selection.exch;
+import static ElementarySorts.Selection.less;
+
+public class Shell {
+    public static void sort(Comparable[] a){
+        int N = a.length;
+
+        int h = 1;
+        while (h < N/3)
+            h = 3*h + 1;    //  1, 4, 13, 40, 121, 364, ...
+
+        while (h >= 1){
+            //  h-sort the array.
+            for (int i = h; i < N; i++){
+                for (int j = i; j >=h && less(a[j], a[j-h]); j -= h)
+                    exch(a, j, j-h);
+            }
+
+            h = h/3;
+        }
+    }
+}
